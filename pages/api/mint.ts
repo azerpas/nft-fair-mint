adbc1838d3a3c4de5ec6adc7ca0339d1c8414788
azerpas <xskylix@hotmail.fr>
2021-11-07 21:53:52 +0100

    mint api route

--- pages/api/mint.ts
+++ pages/api/mint.ts
@@ -1,11 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import web3 from 'web3';
+import crypto from 'crypto';
 
-type Data = {
-    message: string
+type Body = {
+    account: string,
+    amount: string,
+    captcha: string
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+type Data = {
+    hash: string,
+    signature: string,
+    nonce: string
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const signerPrivateKey = process.env.SIGNER_PRIVATE_KEY;
+    const { account, amount } = req.body;
+
+    // TODO: captcha support
+
+    const nonce = crypto.randomBytes(9).toString("base64");
+
+    const content = web3.utils.soliditySha3(
+        {
+            type: "address", 
+            value: account
+        }, 
+        {
+            type: "uint256",
+            value: amount
+        }, 
+        {
+            type: "string",
+            value: nonce
+        }
+    );
+    const Web3 = new web3();
     // Generate the signature
-    // Send to smart-contract
-    res.status(200).json({ message: 'Successfully minted !' })
+    const { messageHash: hash, signature } = await Web3.eth.accounts.sign(content, signerPrivateKey)
+    // Send back
+    res.status(200).json({ hash, signature, nonce })
 }
