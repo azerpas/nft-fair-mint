980c2fe324aeea83425156c7628945584c6de91c
azerpas <xskylix@hotmail.fr>
2021-11-07 23:49:28 +0100

    Started to work on front-end

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+ 
 const Index = () => {
-    return <div>Welcome to my NFT app 👋</div>
+    const [metamaskError, setMetamaskError] = useState();
+
+    const mint = async () => {
+        const provider = await detectEthereumProvider();
+        if (provider) {
+            let accounts = [];
+            try {
+                accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            } catch (error) {
+                console.log(error);
+                if(error.message.includes("MetaMask"))
+                    setMetamaskError(error.message);
+                    throw new Error(error.message);
+            }
+            console.log(accounts);
+            // From now on, this should always be true:
+            // provider === window.ethereum
+            startApp(provider); // initialize your app
+        } else {
+            console.log('Please install MetaMask!');
+        }
+    }
+    return (
+        <div>
+            <h1>Welcome to my NFT app 👋</h1>
+            <button onClick={mint}>Mint</button>
+            <p>{metamaskError}</p>
+        </div>
+    )
 }
   
 export default Index  
---
+++ window.d.ts
@@ -0,0 +1,4 @@
+declare interface Window {
+    readonly ethereum: any | undefined;
+    readonly web3: unknown | undefined;
+}
